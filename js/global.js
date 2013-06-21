df1d220f31005816d26f280a3a73796697675846
Carlo Lai <ckl21@sfu.ca>
2013-06-20 18:49:59 -0700

    menu js

--- js/global.js
+++ js/global.js
@@ -1,6 +1,15 @@
 
 
 $(document).ready(function() {
-
-    
+    var menu = 0;
+	$('#dropdown').click(function(){
+	if(menu == 0){
+		$('header').css({"height":"auto"});
+		menu = 1;
+		}else{
+		$('header').css({"height":"68"});
+		menu = 0;
+		}
 });
+});
+
