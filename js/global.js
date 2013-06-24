fbe628a7bce5d0441eca13fe933347f54d4a53bf
Carlo Lai <ckl21@sfu.ca>
2013-06-23 23:15:23 -0700

    added home page

--- js/global.js
+++ js/global.js
@@ -1,25 +1,32 @@
 
 $(document).ready(function() {
 	var menu;
-	if ($(window).width() < 500) {
+	if ($(window).width() < 560) {
 
 		$("header").css({height : 68});
 		menu = 0;
 	}
 	$(window).resize(function() {
-		if ($(window).width() < 500) {
+		if ($(window).width() < 560) {
 			$("header").css({height : 68});
 			menu = 0;
 		}
 	});
 	$('#dropdown').click(function(){
 	if(menu == 0){
-		$('header').stop().animate({height: 267});
+		$('header').stop().animate({height: 316});
 		menu = 1;
 		}else{
 		$('header').stop().animate({height: 68});
 		menu = 0;
 		}
 });
+
+	$(function(){
+		  $("#slides").slidesjs({
+			width: 1280,
+			height: 420
+		  });
+		});
 });
 
